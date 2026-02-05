678742d9b67151736bcbe9dbf207fb86fdefd131
Simón Villafañe <simonvillafanee@gmail.com>
2026-02-04 21:22:51 -0300

    feat(overlay + keyboard): tetrimoes detecting key events to move left and right

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -3,6 +3,10 @@
 #include <array>
 #include <chrono>
 #include <thread>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 using clock_type = std::chrono::steady_clock;
 using seconds_f  = std::chrono::duration<float>;
@@ -14,13 +18,84 @@ using seconds_f  = std::chrono::duration<float>;
 
 inline void clear()
 {
-    std::cout << "\033[2J";
+    std::cout << "\033[H\033[2J";
 }
 
-inline void home_cursor()
+inline void hideCursor()
+{
+    std::cout << "\033[?25l";
+}
+
+inline void restoreCursor()
+{
+    std::cout << "\033[?25h";
+}
+
+inline void homeCursor()
 {
     std::cout << "\033[H";
 }
+
+termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode()
+{
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    
+    termios raw = orig_termios;
+    
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG); // local modes
+    raw.c_iflag &= ~(IXON | ICRNL);         // input modes
+//    raw.c_oflag &= ~(OPOST);                // output modes
+    
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void setNonBlocking()
+{
+    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+}
+
+enum Key { NONE = -1, UP, DOWN, RIGHT, LEFT, SPACEBAR, ESC, C };
+
+bool readKey(Key& outKey)
+{
+    outKey = Key::NONE;
+
+    char c;
+    if (read(STDIN_FILENO, &c, 1) != 1)
+        return true;
+
+    if (c == '\x1B') {
+        char seq[2];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return false;
+        if (seq[0] != '[') return false;
+
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return false;
+
+        switch (seq[1]) {
+            case 'A': outKey = UP;    return true;
+            case 'B': outKey = DOWN;  return true;
+            case 'C': outKey = RIGHT; return true;
+            case 'D': outKey = LEFT;  return true;
+            default:  return false;
+        }
+    }
+
+    if (c == ' ') outKey = SPACEBAR;
+    else if (c == 'c' || c == 'C') outKey = C;
+
+    return true;
+}
+
 
 // T, S, Z, L, J, O, I
 
@@ -29,7 +104,10 @@ const std::array<std::array<std::string, 3>, 2> L{{
     { "■", "■", "■" },
 }};
 
-//const char tile = u8'■';
+struct Axis {
+    int begin;
+    int end;
+};
 
 constexpr short TARGET_FPS   = 60;
 constexpr auto  FRAME_TIME   = std::chrono::duration<double>(1.0 / TARGET_FPS);
@@ -37,8 +115,8 @@ constexpr short BOARD_WIDTH  = 10;
 constexpr short BOARD_HEIGHT = 22; // 20 + 2 (2 hidden rows)
 
 std::array<std::array<std::string, BOARD_WIDTH>, BOARD_HEIGHT> board{{
-    {" "," "," ","3","4","5"," "," "," "," "},
-    {" "," "," ","6","7","8"," "," "," "," "}, // hidden rows
+    {" "," "," "," "," "," "," "," "," "," "},
+    {" "," "," "," "," "," "," "," "," "," "}, // hidden rows
     
     {" "," "," "," "," "," "," "," "," "," "},
     {" "," "," "," "," "," "," "," "," "," "},
@@ -62,54 +140,76 @@ std::array<std::array<std::string, BOARD_WIDTH>, BOARD_HEIGHT> board{{
     {" "," "," "," "," "," "," "," "," "," "}
 }};
 
-void update(double dt)
+bool running = true;
+auto previous = clock_type::now();
+
+const short x1 = std::floor(( BOARD_WIDTH - 1 ) / 2) - std::floor(( L[0].size() - 1 ) / 2);
+const short x2 = x1 + L[0].size() - 1;
+Axis x = {x1, x2};
+Axis y = {0, 1};
+
+Key key;
+
+void handleKey(Key &key)
 {
-    
+    switch (key) {
+        case Key::LEFT:
+            if (x.begin <= 0) break;
+            x.begin = x.begin - 1;
+            x.end = x.end - 1;
+            break;
+        case Key::RIGHT:
+            if (x.end >= BOARD_WIDTH - 1) break;
+            x.begin = x.begin + 1;
+            x.end = x.end + 1;
+            break;
+        default:
+            break;
+    }
 }
 
-struct Axis {
-    int begin;
-    int end;
-};
+void update(double dt)
+{
+    if (!readKey(key))
+        running = false;
+    
+    handleKey(key);
+}
 
 void render()
 {
-    home_cursor();
+    homeCursor();
     
-    const short x1 = std::floor(( BOARD_WIDTH - 1 ) / 2) - std::floor(( L[0].size() - 1 ) / 2);
-    const short x2 = x1 + L[0].size() - 1;
-    Axis x = {x1, x2};
-    Axis y = {0, 1};
-//    
-//    std::cout << x.begin << "->" << x.end << '\n';
-//    std::cout << y.begin << "->" << y.end << '\n';
-    
-    int k = 0;
-    for (int i = y.begin; i <= y.end; ++i) {
-        for (int j = x.begin; j <= x.end; ++j, ++k) {
-            board[i][j] = L[i][k];
-        }
-        k = 0;
-    }
-    
-    for (const auto& row : board) {
+    std::cout << "\n\n";
+    for (int r = 0; r < BOARD_HEIGHT; ++r) {
         std::cout << "│";
-        for (const auto& cell : row) {
+        for (int c = 0; c < BOARD_WIDTH; ++c) {
+            std::string cell = board[r][c];
+            
+            if (r >= y.begin && r <= y.end &&
+                c>= x.begin && c <= x.end
+            ) {
+                int localY = r - y.begin;
+                int localX = c - x.begin;
+                cell = L[localY][localX];
+            }
+
             std::cout << cell;
         }
         std::cout << "│\n";
     }
-    std::cout << "╰──────────╯" << '\n';
+    std::cout << "╰——————————╯" << '\n';
 }
 
 auto main(int argc, const char * argv[]) -> int
 {
     std::cout.setf(std::ios::unitbuf);
     
+    hideCursor();
+    enableRawMode();
+    setNonBlocking();
+    
     clear();
-
-    bool running = true;
-    auto previous = clock_type::now();
 
     while (running) {
         auto frame_start = clock_type::now();
@@ -126,6 +226,8 @@ auto main(int argc, const char * argv[]) -> int
         if (work_time < FRAME_TIME)
             std::this_thread::sleep_for(FRAME_TIME - work_time);
     }
+    
+    restoreCursor();
     
     return 0;
 }
